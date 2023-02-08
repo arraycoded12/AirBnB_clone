f537fa9de3bebe725153a766505ebe93d993a7e7
arraycoded12 <arrayadelayo001@gmail.com>
2023-02-08 03:45:50 -0800

    Added all tasks

---
+++ models/user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""This module creates a User class"""
+
+from models.base_model import BaseModel
+
+
+
+
+
+class User(BaseModel):
+
+        """Class for managing user objects"""
+
+
+
+            email = ""
+
+                password = ""
+
+                    first_name = ""
+
+                        last_name = ""
