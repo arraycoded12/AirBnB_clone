dbb399443cef08d3188f8811a287bc1286db33de
Ealimah11 <112158775+Ealimah11@users.noreply.github.com>
2023-02-11 09:36:03 +0100

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-
 """This module creates a Amenity class"""
-
-
 
 from models.base_model import BaseModel
 
 
-
-
-
 class Amenity(BaseModel):
+    """Class for managing amenity objects"""
 
-        """Class for managing amenity objects"""
-
-
-
-            name = ""
+    name = ""
