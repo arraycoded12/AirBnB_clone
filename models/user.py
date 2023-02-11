7b497b19fe65145044739e3adecdab2192478521
Ealimah11 <112158775+Ealimah11@users.noreply.github.com>
2023-02-11 09:40:07 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-
 """This module creates a User class"""
-
 from models.base_model import BaseModel
 
 
-
-
-
 class User(BaseModel):
+    """Class for managing user objects"""
 
-        """Class for managing user objects"""
-
-
-
-            email = ""
-
-                password = ""
-
-                    first_name = ""
-
-                        last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
