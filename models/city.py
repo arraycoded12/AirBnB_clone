58a18de061c2a84041890f78d088ba8237d5b9ef
Ealimah11 <112158775+Ealimah11@users.noreply.github.com>
2023-02-11 09:37:41 +0100

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,21 +1,11 @@
 #!/usr/bin/python3
-
 """This module creates a User class"""
-
-
 
 from models.base_model import BaseModel
 
 
-
-
-
 class City(BaseModel):
+    """Class for managing city objects"""
 
-        """Class for managing city objects"""
-
-
-
-            state_id = ""
-
-                name = ""
+    state_id = ""
+    name = ""
